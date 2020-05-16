a874ed029538837b3e8b50830f031522b614b5fa
Anthony Bandolon <anthony@bandolon.me>
2020-05-16 21:45:41 +0200

    g

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,6 @@ class PostsController < ApplicationController
     req.body = post.to_json
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       http.request(req)
-
-    redirect_back fallback_location: posts_path
     end
 
   end
